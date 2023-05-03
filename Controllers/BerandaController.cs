7eb179ba0565b3ba7e1de7fdf0ac19dbbb2e0e8b
elizantonius <lonecode1777@gmail.com>
2023-05-03 23:33:23 +0700

    Halaman Beranda OTW

---
+++ Controllers/BerandaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace siparkgo.Controllers
+{
+    public class BerandaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
